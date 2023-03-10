b67da2a3ee980c39b6a7dac7aa5f824b4c6d5904
michaapyr <michapyr@gmail.com>
2023-03-10 17:24:05 +0100

    index

--- kalkulator/views.py
+++ kalkulator/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
 
+#def index(request):
+#    return HttpResponse("Hello, world. You're at the kalkulator index.")
+
 def index(request):
-    return HttpResponse("Hello, world. You're at the kalkulator index.")
+
+    template = loader.get_template('index.html')
+    return HttpResponse(template.render())
